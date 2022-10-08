82fa1f28e8774257d8f46a7e81c83b4614feec3f
Diyaz Yakubov <diyaz.yakubov@wunderdog.fi>
2022-10-08 11:59:59 +0300

    added SeedData function

--- src/backend/Utils/EntriesService.cs
+++ src/backend/Utils/EntriesService.cs
@@ -27,6 +27,30 @@ namespace backend.Utils
             new EntryModel{ UserName="user2", ProductName = "Apple", Calories = 10, ConsumedAt = new DateTime(2022,11,1,10,30,00)},
         };
 
+        public static void SeedData()
+        {
+            for (int i = 1; i < 30; i++)
+            {
+                for (int j = 1; j < 5; j++)
+                {
+                    _entries.Add(new EntryModel
+                    {
+                        UserName = "user1",
+                        ProductName = "Banana",
+                        Calories = 110,
+                        ConsumedAt = new DateTime(2020, 7, i, j*2, 30, 00)
+                    });
+                    _entries.Add(new EntryModel
+                    {
+                        UserName = "user2",
+                        ProductName = "Banana",
+                        Calories = 140,
+                        ConsumedAt = new DateTime(2021, 7, i, j * 2, 30, 00)
+                    });
+                }
+            }
+        }
+
         public static IEnumerable<EntryModel> GetEntries(string userName)
         {
             return _entries.Where(p => p.UserName == userName)
